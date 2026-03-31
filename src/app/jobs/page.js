019e173f0f8ddf7cd0aab6e8aed89850d4a80aec
Anjali Singh <anjalikhushisingh0605@gmail.com>
2026-03-31 16:37:27 +0530

    added candidate and recruiter dashboard pages

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -1,4 +1,4 @@
-import { fetchProfileAction } from "@/actions";
+import { fetchJobActionForRecruiter, fetchProfileAction, fetchJobActionForCandidate, fetchJobApplicationForCandidateAction, fetchJobApplicationForRecruiter } from "@/actions";
 import JobListing from "@/components/jon-listing";
 import { currentUser } from "@clerk/nextjs/server";
 
@@ -6,10 +6,20 @@ async function JobPages(){
     const user = await currentUser();
     const profileInfo = await fetchProfileAction(user?.id)
 
+    const jobList = profileInfo?.role === 'candidate'
+      ? await fetchJobActionForCandidate()
+     : await fetchJobActionForRecruiter(user?.id);
+
+     const getJobApplicationList = profileInfo?.role === 'candidate'
+     ? await fetchJobApplicationForCandidateAction(user?.id)
+     : await fetchJobApplicationForRecruiter(user?.id)
+
     return(
       <JobListing
       user={JSON.parse(JSON.stringify(user))}
       profileInfo={profileInfo}
+      jobList = {jobList}
+      jobApplications={getJobApplicationList}
       />
     )
 }
